d878f7c772c3b3a49bb43d10a09a8c69692ba980
Madli Reiljan <madlireiljan@gmail.com>
2024-10-17 13:20:31 +0300

    Create navigation between screens and tabs

---
+++ app/(tabs)/Splash/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {Image, Pressable, Text, View} from 'react-native'
+import {styles} from "./styles"
+import Button from "../../../components/Button"
+import { router } from 'expo-router'
+
+const Splash = ({navigation}) => {
+    console.log('navigation => ', navigation)
+
+    const onSignup = () => {
+        navigation.navigate('Signup')
+    }
+    const onSignin = () =>  {
+        navigation.navigate('Signin')
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image  resizeMode="contain" style={styles.image} source={require('../../../assets/images/splash_image.png')}/>
+            
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>You'll find</Text>
+                <Text style={[styles.innerTitle, styles.title]}> All you need
+                </Text>
+                <Text style={styles.title}>Here!</Text>
+            </View>
+
+            <Button onPress={onSignup} title="Sign up" />
+            
+            <Pressable onPress={onSignin} hitSlop={20}>
+                <Text style={styles.footerText} onPress={() => router.push('/(tabs)/signin')} >Sign In</Text>
+            </Pressable>
+        </View>
+    )
+}
+export default Splash
